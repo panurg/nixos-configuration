24b6f4caca06469dee20fd2bfeeb4844f3c6dfbf
Alexander Abrosimov <alexander.n.abrosimov@gmail.com>
2020-10-15 01:30:26 +0300

    pawn: prepare derivation for nixpkgs

--- packages/pawn.nix
+++ packages/pawn.nix
@@ -1,14 +1,16 @@
 { stdenv, fetchzip, pkgs }:
 
 stdenv.mkDerivation rec {
-  name = "pawn";
-  version = "3.10.10";
+  pname = "pawn";
+  version = "4.0.5749";
+
   src = fetchzip {
-    url = "https://www.compuphase.com/pawn/pawn-4.0.5749.zip";
+    url = "https://www.compuphase.com/${pname}/${pname}-${version}.zip";
     sha256 = "1qj2n0xql2bmgvng7q7xf7113bgi2kqgaq9jp0dmys3nc3vm123f";
-    stripRoot = false;
   };
-  nativeBuildInputs = with pkgs; [ cmake gcc_multi ];
+  buildInputs = with pkgs; [ cmake gcc_multi ];
+  enableParallelBuilding = true;
+
   installPhase = ''
     mkdir -p $out/bin
     cp pawncc $out/bin
@@ -16,23 +18,34 @@ stdenv.mkDerivation rec {
     cp pawndisasm $out/bin
     cp pawndbg $out/bin
     cp pawnrun $out/bin
+    mkdir -p $out/lib
+    cp *.so $out/lib
   '';
-  # cmakeFlags = [
-  #   "../source/compiler"
-  #   "-DCMAKE_C_FLAGS=-m32"
-  #   "-DCMAKE_BUILD_TYPE=Release"
-  # ];
-  # buildInputs = [ pkgs.cmake ];
-  # outputs = [ "lib" "bin" ]
-  # buildPhase = ''
-  #   mkdir build
-  #   cd build
-  #   echo $(pwd)
-  #   cmake ../source/compiler -DCMAKE_C_FLAGS=-m32 -DCMAKE_BUILD_TYPE=Release
-  #   make
-  # '';
-  # installPhase = ''
-  #   mkdir -p $out/bin
-  #   cp foo $out/bin
-  # '';
+
+  meta = with stdenv.lib; {
+    description = "A simple, typeless, 32-bit “scripting” language with a C-like syntax";
+    longDescription = ''
+      Pawn is a simple, typeless, 32-bit extension language with a C-like syntax.
+      The Pawn compiler outputs P-code (or bytecode) that subsequently runs on an
+      abstract machine. Execution speed, stability, simplicity and a small footprint
+      were essential design criterions for both the language and the abstract
+      machine.
+    '';
+    homepage = https://www.compuphase.com/pawn/pawn.htm;
+    changelog = https://www.compuphase.com/pawn/pawnhistory.htm;
+    license = [
+      licenses.asl20
+      {
+        fullName = "An exception clause to explicitly permit static linking of the library to commercial application";
+        url = "https://www.compuphase.com/pawn/pawn.htm#APACHE_EXCEPTION";
+      }
+    ];
+    maintainers = [
+      {
+        name = "Alexander Abrosimov";
+        email = "alexander.n.abrosimov@gmail.com";
+      }
+    ];
+    platforms = platforms.all;
+  };
 }
