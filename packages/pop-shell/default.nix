f43993e257adf04ad74bb505353f65de86b43d71
Alexander Abrosimov <alexander.n.abrosimov@gmail.com>
2020-11-05 16:10:44 +0300

    pop-shell: bump version

--- packages/pop-shell/default.nix
+++ packages/pop-shell/default.nix
@@ -2,15 +2,15 @@
 
 stdenv.mkDerivation rec {
   pname = "pop-shell";
-  version = "2020-10-16";
+  version = "2020-11-05";
 
   uuid = "pop-shell@system76.com";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "shell";
-    rev = "a11d3c34db01987bb716b8b127b2b889130a4fc1";
-    sha256 = "0wyx9yg6afxh7cqirarcbsqmqqdfhrxwqnafjdxxsn4aywajx5p8";
+    rev = "9f0976f1f9bf6e3d548fb74fddf86f3c15733311";
+    sha256 = "09fgfzdlrjwzqcqzr60jhpxjf37c4x0w28a0j6nsv9njwjmzggm0";
   };
 
   nativeBuildInputs = [ glib nodePackages.typescript ];
